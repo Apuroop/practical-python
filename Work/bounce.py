3dc74c867155547be5fc1ecdbd4fabe9a203ee62
Apuroop <11079746+Apuroop@users.noreply.github.com>
2020-08-10 12:56:17 +0530

    Completed Ex. 1.5. Noticed interesting case of %f & round function

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+currentHeight = 100
+
+# e is basically the co-efficient of restitution. At times, I miss Physics.
+e = (3/5) 
+
+for i in range (0, 10):
+	currentHeight = round(currentHeight*e,4)
+	print("Height after bounce "+str(i+1)+" is "+str(currentHeight))
