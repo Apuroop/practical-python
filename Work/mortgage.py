c364005ac5014fe800d351169957f65d64b98ada
Apuroop Sethupathy <11079746+Apuroop@users.noreply.github.com>
2020-10-05 03:02:41 +0530

    Updated prints to use F strings

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -33,9 +33,10 @@ while principal > 0:
 			total_paid = total_paid + total_amount_due
 			amount_paid_per_month = total_amount_due
 	current_month += 1
-	print("Month:\t", current_month, sep="\t")
-	print("Installment:", amount_paid_per_month, sep="\t")
-	print("Paid:\t", total_paid, sep="\t")
-	print("Due:\t", principal, sep="\t")
-	print("---")
+	print(f'{current_month} Month(s) {amount_paid_per_month: 0.2f} Installment {total_paid: 0.2f} Paid {principal: 0.2f} Due')
+	# print("Month:\t", current_month, sep="\t")
+	# print("Installment:", amount_paid_per_month, sep="\t")
+	# print("Paid:\t", total_paid, sep="\t")
+	# print("Due:\t", principal, sep="\t")
+	# print("---")
 print("End of Statement")
