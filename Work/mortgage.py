373662b044dff7a3936857b928fec320708984a4
Apuroop Sethupathy <11079746+Apuroop@users.noreply.github.com>
2020-10-05 02:31:45 +0530

    Completed Mortgage example

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,41 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_amount_per_month = 1000
+extra_amount_start_month = 61
+extra_amount_end_month = 108
+current_month = 0
+
+while principal > 0:
+	amount_paid_per_month = 0
+	total_amount_due = principal * (1+rate/12)
+	if((current_month >= extra_amount_start_month) & (current_month<= extra_amount_end_month)):
+		amount_paid_per_month = (payment + extra_amount_per_month)
+		if (amount_paid_per_month < total_amount_due):
+			principal = principal * (1+rate/12) - amount_paid_per_month
+			total_paid = total_paid + amount_paid_per_month
+		else:
+			principal = principal * (1+rate/12) - total_amount_due
+			total_paid = total_paid + total_amount_due
+			amount_paid_per_month = total_amount_due
+	else:
+		amount_paid_per_month = payment
+		if (amount_paid_per_month < total_amount_due):
+			principal = principal * (1+rate/12) - amount_paid_per_month
+			total_paid = total_paid + amount_paid_per_month
+		else:
+			principal = principal * (1+rate/12) - total_amount_due
+			total_paid = total_paid + total_amount_due
+			amount_paid_per_month = total_amount_due
+	current_month += 1
+	print("Month:\t", current_month, sep="\t")
+	print("Installment:", amount_paid_per_month, sep="\t")
+	print("Paid:\t", total_paid, sep="\t")
+	print("Due:\t", principal, sep="\t")
+	print("---")
+print("End of Statement")
