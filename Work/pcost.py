b4f9f35f866e96fb6ec8310d0e0ca39733e8f04d
Apuroop Sethupathy <11079746+Apuroop@users.noreply.github.com>
2020-10-13 09:48:01 +0530

    Updated the program to run from the CLI + added Error Handling + used CSV. This should have been 3 different commits

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,33 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
-data_variable = open("Data/portfolio.csv", "rt")
-data_header = next(data_variable).split(",")
+def price_calculator(sourceFileName):
+	"This new function takes in a file with the columns Stock Name, Stock Count & Stock Price. It returns the total value of the portfolio"
+	data_variable = open(sourceFileName, "rt")
+	data_rows = csv.reader(data_variable)
+	data_header = next(data_rows)
+	stock_name=[]
+	stock_count=[]
+	stock_price=[]
+	for row in data_rows:
+		stock_name.append(row[0])
+		stock_count.append(row[1])
+		stock_price.append(row[2])
 
-stock_name=[]
-stock_count=[]
-stock_price=[]
-for line in data_variable:
-	row_data = line.split(",")
-	stock_name.append(row_data[0])
-	stock_count.append(row_data[1])
-	stock_price.append(row_data[2].strip())
+	total_price = 0
+	for i in range (0, len(stock_count)):
+		try:
+			total_price += (int(stock_count[i])*float(stock_price[i]))
+		except ValueError:
+			print("Missing Values for " +stock_name[i])
 
-total_price = 0
-for i in range (0, len(stock_count)):
-	total_price += (int(stock_count[i])*float(stock_price[i]))
+	return(total_price)
 
-print(total_price)
+if (len(sys.argv) == 2):
+	cost = price_calculator(sys.argv[1])
+	print("Total portfolio value: "+ str(cost))
+else:
+	print("Incorrect values submitetd. Please enter the arguments as python <program name.py> <path to file>")
