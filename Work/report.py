7e19ea45091b36224d8b816eb681e40c70b14862
Apuroop <11079746+Apuroop@users.noreply.github.com>
2021-07-18 18:06:29 +0530

    Updated report to check current stock prices & update if the user can retire or not

--- Work/report.py
+++ Work/report.py
@@ -16,12 +16,39 @@ def read_portfolio(FilePath):
 			portfolio.append(current_values)
 	return (portfolio)
 
+def read_current_prices(FilePath):
+	with open(FilePath, "rt") as f:
+		rows = csv.reader(f)
+		price_dict = {}
+		for row in rows:
+			# added this because the file might contain some empty lines which cause this to break. 
+			# the better approach might be to try-catch this instead. 
+			# will do that as the next example
+			if (len(row) == 0): 
+				continue
+			else:
+				price_dict[row[0]] = float(row[1])
+	return (price_dict)
+
 current_portfolio = read_portfolio("Data/portfolio.csv")
 
-total_portfolio_value = 0
+current_stock_prices = read_current_prices("Data/prices.csv")
+
+# pprint(current_stock_prices)
+
+starting_portfolio_value = 0
+current_portfolio_value = 0
 
 for stocks in current_portfolio:
-	total_portfolio_value += stocks["Stock Count"]*stocks["Stock Price"]
+	# print(stocks)
+	starting_portfolio_value += stocks["Stock Count"]*stocks["Stock Price"]
+	current_portfolio_value += current_stock_prices[stocks["Stock Name"]]*stocks["Stock Count"]
 
+portfolio_status = current_portfolio_value-starting_portfolio_value
 
-print ("Total value of Current Portfolio: ", total_portfolio_value)
+if (portfolio_status <=0):
+	print("Your current loss is: ", portfolio_status)
+	print("You cannot retire! :( ")
+else:
+	print("Your current gain is: ", portfolio_status)
+	print("You could possibly retire! :D")
