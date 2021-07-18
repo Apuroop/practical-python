38577f85cf3fc223bcd3b1f952fef642830c5d3f
Apuroop <11079746+Apuroop@users.noreply.github.com>
2021-07-18 17:23:00 +0530

    Report made using Dicts

--- Work/report.py
+++ Work/report.py
@@ -1,9 +1,10 @@
 # report.py
 #
 # Exercise 2.4
-# For this attempt, I will be making a report that stores a portfolio in tuples & displays the total portfolio
+# For this attempt, I will be making a report that stores a portfolio in dicts & displays the total portfolio
 
 import csv
+from pprint import pprint
 
 def read_portfolio(FilePath):
 	portfolio = []
@@ -11,7 +12,7 @@ def read_portfolio(FilePath):
 		rows = csv.reader(f)
 		headers = next(rows)
 		for row in rows:
-			current_values = (row[0], int(row[1]), float(row[2]))
+			current_values = {"Stock Name": row[0], "Stock Count": int(row[1]), "Stock Price": float(row[2])}
 			portfolio.append(current_values)
 	return (portfolio)
 
@@ -19,7 +20,8 @@ current_portfolio = read_portfolio("Data/portfolio.csv")
 
 total_portfolio_value = 0
 
-for stock_name, stock_count, stock_price in current_portfolio:
-	total_portfolio_value += stock_count*stock_price
+for stocks in current_portfolio:
+	total_portfolio_value += stocks["Stock Count"]*stocks["Stock Price"]
 
-print("Total Value of the portfolio :", total_portfolio_value)
+
+print ("Total value of Current Portfolio: ", total_portfolio_value)
